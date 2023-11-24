d2dd5eecfbe70e01fa11078642b3df94be314194
Alexander G. Morano <amorano@gmail.com>
2023-11-24 01:48:46 -0800

    moving cmaera towards generic class priliniary rtsp support

--- sup/camera.py
+++ sup/camera.py
@@ -12,21 +12,30 @@ from typing import Any
 loginfo = print
 
 class WebCamera:
-
-    def __init__(self, idx:int=0, width:int=640, height:int=480, fps:float=30) -> None:
-        self.__idx = idx
-        self.__ret = False
-        self.__frame = None
-        self.__paused = False
-        self.__running = False
-        self.__thread = None
+    def __init__(self, url:[int|str]=None, width:int=640, height:int=480, fps:float=30) -> None:
         self.__camera = None
+        self.__thread = None
+        self.__running = False
+        self.__paused = False
+        self.__url = url
+        self.__ret = False
+        self.__frame = np.zeros((width, height, 3), dtype=np.uint8)
 
-        for x in [cv2.CAP_DSHOW, cv2.CAP_V4L, cv2.CAP_ANY]:
-            self.__camera = cv2.VideoCapture(idx, x)
-            if not self.__camera.isOpened():
-                raise Exception(f"CAMERA ({idx}) FAILED")
-            break
+        found = False
+        try:
+            url = int(url)
+            for x in [cv2.CAP_DSHOW, cv2.CAP_V4L, cv2.CAP_ANY]:
+                self.__camera = cv2.VideoCapture(url, x)
+                if self.__camera.isOpened():
+                    found = True
+                    break
+
+        except ValueError as _:
+            self.__camera = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
+            found = self.__camera.isOpened()
+
+        if not found:
+            raise Exception(f"[WebCamera] FAILED ({url}) ")
 
         self.__camera.set(cv2.CAP_PROP_BUFFERSIZE, 2)
         self.__size = (width, height)
@@ -35,7 +44,7 @@ class WebCamera:
         self.width = width
         self.height = height
         self.fps = fps
-        loginfo(f"[WebCamera] CAMERA ({idx}) INIT")
+        loginfo(f"[WebCamera] INIT ({url}) ")
 
     def __process(self) -> None:
         while self.__running:
@@ -61,7 +70,7 @@ class WebCamera:
         if self.__camera:
             if self.__camera.isOpened():
                 self.__camera.release()
-                loginfo(f"[WebCamera] CAMERA ({self.__idx}) RELEASED")
+                loginfo(f"[WebCamera] RELEASED ({self.__url})")
 
     def capture(self) -> None:
         self.__paused = False
@@ -69,8 +78,8 @@ class WebCamera:
         self.__thread = threading.Thread(target=self.__process, daemon=True)
         self.__thread.start()
         # give it a beat
-        time.sleep(0.75)
-        loginfo(f"[WebCamera] CAMERA ({self.__idx}) CAPTURE")
+        time.sleep(0.4)
+        loginfo(f"[WebCamera] CAPTURE ({self.__url})")
 
     def stop(self) -> None:
         self.__paused = True
@@ -78,7 +87,7 @@ class WebCamera:
         if self.__thread:
             self.__thread.join()
         self.__thread = None
-        loginfo(f"[WebCamera] CAMERA ({self.__idx}) STOPPED")
+        loginfo(f"[WebCamera] STOPPED ({self.__url})")
 
     @property
     def paused(self) -> bool:
@@ -133,90 +142,85 @@ class WebCamera:
         return self.__camera.isOpened()
 
 class CameraManager:
+
+    CAMS = {}
+
     @classmethod
-    def check_cam_async(cls, idx: int) -> tuple[int, WebCamera | None]:
+    def check_cam_async(cls, idx: int) -> None:
         try:
             camera = WebCamera(idx)
         except:
-            return idx, None
+            return
 
         camera.capture()
         if not camera.opened:
-            return idx, None
-
-        ret, _ = camera.frameResult
+            return
+        frame = None
+        while frame is None or not ret:
+            ret, frame = camera.frameResult
         camera.stop()
-        if ret:
-            return idx, camera
+        CameraManager.CAMS[idx] = camera
 
     @classmethod
-    def devicescan(cls) -> dict[int, WebCamera]:
+    def devicescan(cls) -> None:
         """Indexes all devices that responded and if they are read-only."""
 
-        idx = 0
-        device = {}
+        CameraManager.CAMS = {}
         start = time.time()
-        # check for X cameras at a time
-        step = 3
-        found = False
 
-        while True:
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                futures = [executor.submit(cls.check_cam_async, i) for i in range(idx, idx + step)]
-                for future in concurrent.futures.as_completed(futures):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(cls.check_cam_async, i) for i in range(4)]
+            for future in concurrent.futures.as_completed(futures):
+                try:
                     idx, camera = future.result()
-                    if camera:
-                        device[idx] = camera
-                    else:
-                        found = True
-
-                if found:
-                    break
-                idx += step
+                    CameraManager.CAMS[idx] = camera
+                except TypeError as _:
+                    pass
 
         loginfo(f"[CameraManager] INIT ({time.time()-start:.4})")
-        return device
 
     def __init__(self) -> None:
-        # scan and link in all cameras?
-        self.__cams = CameraManager.devicescan()
-        loginfo(f"[CameraManager] CAMS {list(self.__cams.keys())}")
+        CameraManager.devicescan()
+        loginfo(f"[CameraManager] CAMS {self.camlist}")
 
     def __del__(self) -> None:
-        for c in self.__cams.values():
-            if c:
-                del c
+        for c in CameraManager.CAMS.values():
+            c.release()
 
     @property
     def camlist(self) -> list[WebCamera]:
-        return list(self.__cams.keys())
+        return list(CameraManager.CAMS.keys())
 
     def frame(self, idx: int) -> Any:
-        if (camera := self.__cams.get(idx, None)) is None:
+        if (camera := CameraManager.CAMS.get(idx, None)) is None:
             return None
         return camera.frame
 
     def capture(self, idx: int) -> None:
-        if (camera := self.__cams.get(idx, None)) is None:
+        if (camera := CameraManager.CAMS.get(idx, None)) is None:
             return
         camera.capture()
 
     def captureAll(self) -> None:
-        for camera in self.__cams.values():
+        for camera in CameraManager.CAMS.values():
             camera.capture()
+
+    def addURL(self, url: str) -> Any:
+        return WebCamera(url=url)
 
 if __name__ == "__main__":
     camMgr = CameraManager()
+    camMgr.addURL("rtsp://rtspstream:804359a2ea4669af4edf7feab36ce048@zephyr.rtsp.stream/pattern")
     camMgr.captureAll()
 
-    width = 1024
+    width = 1280
     height = 384
-    width2 = width // 2
+    width2 = width // 3
     height2 = height #// 2
 
     frame = np.zeros((height, width, 3), dtype=np.uint8)
     while True:
-        for x in range(2):
+        for x in camMgr.camlist:
             if (chunk := camMgr.frame(x)) is not None:
                 chunk = cv2.resize(chunk, (width2, height))
                 col = x % 2
@@ -226,5 +230,7 @@ if __name__ == "__main__":
         val = cv2.waitKey(1) & 0xFF
         if val == ord('q'):
             break
+        elif val == ord('k'):
+            camMgr.addURL("rtsp://rtspstream:804359a2ea4669af4edf7feab36ce048@zephyr.rtsp.stream/pattern")
 
     cv2.destroyAllWindows()
